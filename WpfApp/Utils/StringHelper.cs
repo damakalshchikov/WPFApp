c3dc44f5078026ac5f8b797318a4432a50a32c16
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-12-10 04:02:20 +0300

    Добавлен класс StringHelper для форматирования строк

---
+++ WpfApp/Utils/StringHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WpfApp.Utils;
+
+
+/*
+ * Класс StringHelper служит для преобразования пользовательского ввода
+ * в такой формат, который можно было бы посчитать с помощью возможностей C#
+ */
+public class StringHelper
+{
+    public static string SanitizeInput(string input)
+    {
+        // Замена символов "+×," на "/*."
+        input =  Regex.Replace(
+            input,
+            @"[÷×,]",
+            match =>
+            {
+                return match.Value switch
+                {
+                    "÷" => "/",
+                    "×" => "*",
+                    "," => ".",
+                    _ => match.Value
+                };
+            }
+        );
+        
+        // Замена символа % на выражение <число>/100
+        input = Regex.Replace(
+            input,
+            @"(\d+(\.\d+)?)%",
+            match =>
+            {
+                var number = match.Groups[1].Value;
+                return $"({number}/100)";
+            }
+        );
+        
+        return input;
+    }
+
+    public static string FormatOutput(string output)
+    {
+        // Замена . на , в output
+        return Regex.Replace(output, @"\.", ",");
+    }
+}
