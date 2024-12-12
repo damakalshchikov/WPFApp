ea2de6280b521458321af463763862e457393381
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-12-12 06:07:39 +0300

    Добавлено доп. окно, которое расширяте функционал калькулятора

---
+++ WpfApp/ScientificWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp;
+
+public partial class ScientificWindow : Window
+{
+    public ScientificWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+            DragMove();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
