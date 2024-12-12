db90d76d6fd1f9aadc3179d89d5d70a946378076
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-12-12 06:12:34 +0300

    1) Добавлен метод, который открывает и закрывает доп. окно с помощью кнопки 2) Изменена логика закрытия главного окна: При закрытии главного окна, доп. окно тоже закрыватеся

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -12,9 +12,11 @@ namespace WpfApp;
 public partial class MainWindow : Window
 {
     private string? _currentInput = "";
+    private ScientificWindow? _scientificWindow;
     public MainWindow()
     {
         InitializeComponent();
+        Closed += MainWindow_Closed;
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
@@ -49,12 +51,36 @@ public partial class MainWindow : Window
         }
     }
     
+    private void OpenScientificWindow(object sender, RoutedEventArgs e)
+    {
+        if (_scientificWindow == null) // Если окно ещё не открыто
+        {
+            _scientificWindow = new ScientificWindow();
+            _scientificWindow.Closed += (s, args) => _scientificWindow = null;
+            _scientificWindow.Show();
+        }
+        else
+        {
+            _scientificWindow.Close(); // Закрываем окно, если оно уже открыто
+            _scientificWindow = null;
+        }
+    }
+
+    
     private void Window_MouseDown(object sender, MouseButtonEventArgs e)
     {
         if (e.LeftButton == MouseButtonState.Pressed)
             DragMove();
     }
-
+    
+    private void MainWindow_Closed(object? sender, EventArgs e)
+    {
+        if (_scientificWindow != null)
+        {
+            _scientificWindow.Close(); // Закрыть окно с математическими функциями
+            _scientificWindow = null;
+        }
+    }
     
     private void CloseButton_Click(object sender, RoutedEventArgs e)
     {
