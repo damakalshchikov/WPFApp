50967bb51d4995f091bbb542009107813dc139e7
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-12-16 09:04:21 +0300

    Добавлены регулярные выражения для математических ф-ий

--- WpfApp/Utils/StringHelper.cs
+++ WpfApp/Utils/StringHelper.cs
@@ -13,19 +13,105 @@ public class StringHelper
 {
     public static string SanitizeInput(string input)
     {
-        // Замена символов "+×," на "/*."
-        input =  Regex.Replace(
+        // Замена π/e на числовое значение
+        input = Regex.Replace(
             input,
-            @"[÷×,]",
+            @"(π|e)",
             match =>
             {
-                return match.Value switch
+                var mathConst = match.Groups[1].Value;
+                
+                switch (mathConst)
                 {
-                    "÷" => "/",
-                    "×" => "*",
-                    "," => ".",
-                    _ => match.Value
-                };
+                    case "π":
+                        return Math.PI.ToString();
+                    case "e":
+                        return Math.E.ToString();
+                    default:
+                        return match.Value;
+                }
+            }
+        );
+        Console.WriteLine(input);
+        
+        input = ReplaceBaseSymbols(input);
+        
+        // Обработка sin, cos и tg
+        input = Regex.Replace(
+            input,
+            @"(sin|cos|tg)\(([^()]+)\)",
+            match =>
+            {
+                var function = match.Groups[1].Value;
+                var expression = match.Groups[2].Value; 
+                var value = new DataTable().Compute(expression, null);
+                
+                switch (function)
+                {
+                    case "sin":
+                        return Math.Sin(Convert.ToDouble(value)).ToString();
+                    case "cos":
+                        return Math.Cos(Convert.ToDouble(value)).ToString();
+                    case "tg":
+                        return Math.Tan(Convert.ToDouble(value)).ToString();
+                    default:
+                        return match.Value;
+                }
+
+            }
+        );
+        
+        input = ReplaceBaseSymbols(input);
+        
+        // Замена ln на вычесленное выражение
+        input = Regex.Replace(
+            input,
+            @"ln\(([^()]+)\)",
+            match =>
+            {
+                var expression = match.Groups[1].Value;
+                var value = new DataTable().Compute(expression, null);
+                
+                var res = Math.Log(Convert.ToDouble(value)).ToString();
+                Console.WriteLine(res);
+
+                return Math.Log(Convert.ToDouble(value)).ToString();
+            }
+        );
+        
+        input = ReplaceBaseSymbols(input);
+        
+        // Замена "²" на выражение (number*number)
+        input = Regex.Replace(
+            input,
+            @"(\d+)²",
+            match =>
+            {
+                var number = match.Groups[1].Value;
+                return $"({number}*{number})";
+            }
+        );
+        
+        // Замена "^" на выражение (base*)
+        input = Regex.Replace(
+            input,
+            @"(\d+|\([^()]+\))\^(\d+|\([^()]+\))",
+            match =>
+            {
+                var number = int.Parse(match.Groups[1].Value);
+                var exponentPart = int.Parse(match.Groups[2].Value);
+                return Math.Pow(number, exponentPart).ToString();
+            }
+        );
+        
+        // Замена "!" на вычесленный факториал
+        input = Regex.Replace(
+            input,
+            @"(\d+)!",
+            match =>
+            {
+                var number = int.Parse(match.Groups[1].Value);
+                return SpecialFunctions.Factorial(number).ToString();
             }
         );
         
